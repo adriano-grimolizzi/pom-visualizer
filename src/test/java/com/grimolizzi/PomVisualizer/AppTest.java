1777d27dcf103163b2d3b8c7dee7c1ac9a22e7c2
Adriano Grimolizzi <adriano.grimolizzi@amadeus.com>
2019-10-14 11:35:43 +0200

    Added unit tests

---
+++ src/test/java/com/grimolizzi/PomVisualizer/AppTest.java
@@ -0,0 +1,14 @@
+package com.grimolizzi.PomVisualizer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void shouldReturnTrue() {
+		assertTrue(true);
+	}
+
+}
